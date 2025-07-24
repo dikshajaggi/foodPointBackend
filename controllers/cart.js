accc3467e98a46718fdee0597968ed2441f6e8ff
cee-diksha <cee.diksha@gmail.com>
2025-07-24 16:43:26 +0530

    cart controllers modified

--- controllers/cart.js
+++ controllers/cart.js
@@ -51,20 +51,55 @@ const addItemToCart = async(req, res, next) => {
         await existingCart.save();
         return res.status(200).json({ msg: "Item added to cart", cart: existingCart });
     }
-    
    } catch (error) {
     next(error)
    }
 }
 
 const removeItemFromCart = async(req, res, next) => {
+   try {
+        const userId = req.user.userId
+        const {itemId} = req.body
+        const userExists = await user.findById(userId)
+        if(!userExists)  return res.status(404).json({ msg: "User not found" });
 
+        const existingCart = await cart.findOne({ user: userId });
+        if (!existingCart) return res.status(404).json({ msg: "Cart not found" });
+        
+        const removedItemIndex = existingCart.items.findIndex(item => item._id.toString() === itemId.toString());
+
+        if (removedItemIndex === -1) return res.status(404).json({ msg: "Item not found in cart" });
+
+        existingCart.items.splice(removedItemIndex, 1);
+        // save cart so pre('save') middleware recalculates totalCost
+        await existingCart.save();
+
+        return res.status(200).json({ msg: "Item removed from cart", cart: existingCart });
+   } catch (error) {
+        next(error)
+   }
 }
 
 // above apis will work for adding / removing / qty inc / qty dec
 
 const clearCart = async(req, res, next) => {
+   try {
+        const userId = req.user.userId
+        const {itemId} = req.body
+        const userExists = await user.findById(userId)
+        if(!userExists)  return res.status(404).json({ msg: "User not found" });
 
+        const existingCart = await cart.findOne({ user: userId });
+        if (!existingCart) return res.status(404).json({ msg: "Cart not found" });
+        
+        existingCart.items = []
+        // save cart so pre('save') middleware recalculates totalCost
+        await existingCart.save();
+
+        return res.status(200).json({ msg: "Cart cleared successfully", cart: existingCart });
+   } catch (error) {
+        next(error)
+   }
 }
 
 
